89eab9dad0322f57bff7baaebc3c87bffd736dae
JuanSpada <spadajuan94@gmail.com>
2018-09-06 20:12:44 -0300

    Menu y footer

--- _head.php
+++ _head.php
@@ -3,21 +3,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
     <title>Black Hawk</title>
 </head>
 <body>
     <header>
-        <head>
-            <ul>
+        <nav>
+            <ul class="nav-izq">
                 <li><a href="index.php">Inicio</a></li>
                 <li><a href="">Productos</a></li>
                 <li><a href="">FAQ</a></li>
                 <li><a href="">¿Quiénes Somos?</a></li>
                 <li><a href="">Contacto</a></li>
             </ul>
-            <ul>
+            <ul class="nav-der">
                 <li><a href="login.php">Iniciar Sesión</a></li>
                 <li><a href="register.php">Registrarse</a></li>
             </ul>
-        </head>
+        </nav>
     </header>
---
+++ register.php
@@ -0,0 +1,10 @@
+<?php include_once("_head.php") ?>
+<!-- Acá empieza el contenido de la página REGISTER -->
+<section>
+
+
+
+
+
+</section>
+<?php include_once("_footer.php") ?>
